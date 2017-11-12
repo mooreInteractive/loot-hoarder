099e3e8c3d6f3ff080da9cfd578bce7d9bbbb6e3
Adam Moore <amooreinteractive@gmail.com>
2017-11-11 21:37:18 -0800

    increment version - data wipe

--- src/main.js
+++ src/main.js
@@ -34,7 +34,7 @@ class Game extends Phaser.Game {
         /***** VERSION NUMBER - UPDATING WILL WIPE PLAYER DATA *************
         /****
         /**/
-        this.version = '0.0.25'; //updated
+        this.version = '0.0.26'; //updated
         /**/
         /****
         *******************************************************************/
